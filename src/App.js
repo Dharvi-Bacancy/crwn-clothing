d5da8ab3be612d469ae0482e9102af68ae144693
Dharvi-Bacancy <dharvi.patel@bacancy.com>
2022-04-21 13:35:48 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,18 @@ import HomePage from './pages/homepage/homepage.component';
 import ErrorPage from './pages/ErrorPage';
 import { Route,Routes } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-const TopicDetail = () => (
-  <div>
-    <h1>
-    TopicDetail PAGE
-    </h1>
-  </div>
-);
 function App() {
   return (
-    
-    
-        <div>
-        <Routes>
-          <Route exact path='/' element={<HomePage />}/>
-          <Route exact path='/shop' element={<ShopPage />}/>
-          <Route path='/topics/:topicId' element={<TopicDetail />}/>
-          <Route path='*' element={<ErrorPage />} />
-        </Routes>
-        </div>
-      
-    
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<HomePage />}/>
+        <Route exact path='/shop' element={<ShopPage />}/>
+        <Route path='*' element={<ErrorPage />} />
+      </Routes>
+    </div>  
   );
 }
 
